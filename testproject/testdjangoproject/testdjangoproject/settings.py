32f4f6361b5c8c49084c0ee0a5b269fb75d7b6f9
pleasantlife <pleasantlife@gmail.com>
2018-04-10 23:39:07 +0900

    180410 django database change(sqlite3 -> postgresql)

--- testproject/testdjangoproject/testdjangoproject/settings.py
+++ testproject/testdjangoproject/testdjangoproject/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'testdjangoproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+# django에서는 기본적으로 제공하는 데이터베이스인 sqlite3 이외에도 postgresql을 사용할 수 있다.
+# 단, 사용을 위해서는 psycopg2 라이브러리를 설치해야한다. (pip install psycopg2)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydatabase',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
